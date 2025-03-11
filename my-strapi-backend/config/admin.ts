8bd5d89cfcf634bcd98769fe2aa1ffaf79335739
iamqazi <qaziobaid7897@gmail.com>
2025-03-11 22:58:31 +0500

    lint fixes

--- my-strapi-backend/config/admin.ts
+++ my-strapi-backend/config/admin.ts
@@ -1,17 +1,24 @@
-export default ({ env }) => ({
+type EnvFunction = (
+  key: string,
+  defaultValue?: string | boolean
+) => string | boolean;
+
+const config = ({ env }: { env: EnvFunction }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env("FLAG_NPS", true) as boolean,
+    promoteEE: env("FLAG_PROMOTE_EE", true) as boolean,
   },
 });
+
+export default config;
--- my-strapi-backend/config/server.ts
+++ my-strapi-backend/config/server.ts
@@ -1,7 +1,16 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+type EnvFunction = {
+  (key: string, defaultValue?: string): string;
+  int: (key: string, defaultValue?: number) => number;
+  bool: (key: string, defaultValue?: boolean) => boolean;
+  array: (key: string) => string[];
+};
+
+const serverConfig = ({ env }: { env: EnvFunction }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
+
+export default serverConfig;
